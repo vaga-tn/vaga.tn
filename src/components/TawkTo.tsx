eaa4f60ce437be670ffa03e31df356d848c44895
Aymen KHEDHRIYA <aymen.khedhriya@gmail.com>
2025-06-14 17:26:39 +0100

    fix: typo

--- src/components/TawkTo.tsx
+++ src/components/TawkTo.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function TawkScript() {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://tawk.to/chat/6848527d77026219101b65aa/1itd84eda";
+    script.src = "https://embed.tawk.to/6848527d77026219101b65aa/1itd84eda";
     script.async = true;
     script.charset = "UTF-8";
     script.setAttribute("crossorigin", "*");
