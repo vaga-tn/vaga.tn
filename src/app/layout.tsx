d8d441654570b5118c9791e043ad9232cf132a3b
aymen khedhriya <aymen4real@gmail.com>
2026-03-22 00:35:30 +0100

    feat: adding GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -47,6 +48,7 @@ export default function RootLayout({
     <html lang="fr" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>
         {children}
+        <GoogleTagManager gtmId="GTM-KNPPVR4L" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
