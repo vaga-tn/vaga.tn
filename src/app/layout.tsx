ddafcc04adb1dcae422f3f13130a1075ab90eb5d
aymen khedhriya <aymen4real@gmail.com>
2026-03-21 22:28:35 +0100

    fix: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,18 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "VAGA - L'architecte moderne de votre infrastructure d'entreprise en Tunisie",
   description: "VAGA est un service fourni par MAAK CORP SUARL représenté par AYMEN KHEDHRIYA; société domiciliée à Av. Yasser Arafet, imm 16, 6ème étage, bureau 2, Sahloul 1, Sousse 4054, numéro de TVA: 1847208/C/A/M/000",
+  icons: [
+    {
+      media: "(prefers-color-scheme: light)",
+      url: "/favicon_dark.png",
+      href: "/favicon_dark.png",
+    },
+    {
+      media: "(prefers-color-scheme: dark)",
+      url: "/favicon_light.png",
+      href: "/favicon_light.png",
+    },
+  ],
 };
 
 export default function RootLayout({
