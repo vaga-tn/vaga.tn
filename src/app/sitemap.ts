5a81ec090be4447ca651ac46c9e65b6e4fc223fe
aymen khedhriya <aymen4real@gmail.com>
2026-03-24 21:17:49 +0100

    chore: sitemap updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,8 +5,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://vaga.tn',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: 'monthly',
       priority: 1,
+    },
+    {
+      url: 'https://vaga.tn/changelog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
     },
   ]
 }
