9c331a76caf56d117fd652077d5a7942bc30c04c
aymen khedhriya <aymen4real@gmail.com>
2026-04-09 00:20:14 +0100

    feat: WIP i18n

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+import createNextIntlPlugin from "next-intl/plugin"
+
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts")
 
 const nextConfig: NextConfig = {
   images: {
@@ -9,6 +12,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-};
+}
 
-export default nextConfig;
+export default withNextIntl(nextConfig)
