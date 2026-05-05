70131e07e1d553b87f7df1885ff63ce5490204c4
aymen khedhriya <aymen4real@gmail.com>
2026-05-05 21:19:43 +0100

    chore: updated changelog

--- src/data/changelog.ts
+++ src/data/changelog.ts
@@ -16,18 +16,46 @@ export interface ChangelogEntry {
 
 export const changelogData: ChangelogEntry[] = [
   {
+    id: "update-3",
+    date: "coming soon",
+    leftTitle: "V1.2",
+    rightTitle: "New Features",
+    description: [],
+    listSection: {
+      title: "",
+      items: [
+        "POS system",
+        "Calendar system",
+      ],
+    }
+  },
+  {
+    id: "update-2",
+    date: "Apr 2026",
+    leftTitle: "V1.0",
+    rightTitle: "Stable release",
+    description: [],
+    listSection: {
+      title: "",
+      items: [
+        "Manage Company (details, addresses, staff, vehicles, equipment...)",
+        "Manage clients & suppliers",
+        "Manage sales & purchases",
+        "Manage assets through the drive feature",
+        "Manage Payments (+ RS)",
+        "Manage documents lifecycle",
+        "Customize default view",
+        "Multi-company support",
+      ],
+    }
+  },
+  {
     id: "update-1",
     date: "Mars 2026",
-    leftTitle: "Lancement de Vaga.tn",
-    rightTitle: "Lancement de Vaga.tn",
+    leftTitle: "Launching Vaga.tn",
+    rightTitle: "Official Website",
     description: [
-      "Vaga.tn est le site web officiel pour l'ERP Tunisien Vaga.",
+      "Our official client facing website, used for capturing leads and providing transparent quote generation tool.",
     ],
-    listSection: {
-      title: "Vaga est un ERP multi-sociétés, self-hosted, développé en Tunisie.",
-      items: [
-        "comming soon"
-      ]
-    }
   }
 ];
