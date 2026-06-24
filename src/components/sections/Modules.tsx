9baa766731eaa6f7e05d80e7aceaefae4776fe2f
aymen khedhriya <aymen4real@gmail.com>
2026-06-24 15:55:21 +0100

    chore: modules section UI refactor

--- src/components/sections/Modules.tsx
+++ src/components/sections/Modules.tsx
@@ -32,31 +32,33 @@ export async function Modules() {
   const sectionT = await getTranslations("sections.apps")
 
   return (
-    <section className="sectionWrapper" id="modules">
+    <section className="bg-[#F3F5F6]" id="modules">
 
-      <SectionTitle title={sectionT("title")} subtitle={sectionT("subtitle")} />
+      <section className="space-y-24 sectionWrapper">
 
-      <div className="grid grid-cols-4 gap-12 mx-auto overflow-hidden w-fit lg:grid-cols-6">
-        {MODULE_KEYS.map((key) => (
-          <div
-            key={key}
-            className="flex flex-col items-center justify-center gap-4 text-center"
-          >
-            <div className="p-4 bg-white border border-solid rounded-md border-zinc-400">
-              <Image
-                src={MODULE_ICONS[key]}
-                alt={`Module ${t(key)} - VAGA ERP Tunisie`}
-                width={64}
-                height={64}
-                className="object-contain w-16 h-16"
-              />
+        <div className="grid grid-cols-4 gap-8 mx-auto overflow-hidden w-fit lg:grid-cols-6">
+          {MODULE_KEYS.map((key) => (
+            <div
+              key={key}
+              className="flex flex-col items-center justify-center gap-4 text-center"
+            >
+              <div className="p-4 bg-white border border-solid rounded-md border-zinc-400">
+                <Image
+                  src={MODULE_ICONS[key]}
+                  alt={`Module ${t(key)} - VAGA ERP Tunisie`}
+                  width={64}
+                  height={64}
+                  className="object-contain w-16 h-16"
+                />
+              </div>
+              <span className="text-sm font-normal text-black capitalize">
+                {t(key)}
+              </span>
             </div>
-            <span className="text-sm font-normal text-black capitalize">
-              {t(key)}
-            </span>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </section>
+
     </section>
   )
 }
